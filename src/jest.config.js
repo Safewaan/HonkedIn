f30448b15a84959b66c468fcd6e38d32d5b6e8d9
Safewaan <Safewaan@users.noreply.github.com>
2023-02-13 11:43:50 -0500

    Revert "add testing dependencies, test configuration files, and sample test"
    This reverts commit a954040e67ca2bd6aeba27384f568db214dc1952.

--- src/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    transform: {
-        "^.+\\.jsx?$": "babel-jest",
-    },
-    moduleFileExtensions: ["js", "jsx"],
-    testMatch: ["**/__tests__/**/*.js?(x)", "**/?(*.)+(spec|test).js?(x)"],
-};
