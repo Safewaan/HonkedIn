e13132d9d5df109fc58741ba428f68d4716ea6f4
c45choi <c45choi@uwaterloo.ca>
2023-02-20 10:18:32 -0500

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,6 +2,23 @@ import React, { useState } from "react"
 import { Card, Button, Alert } from "react-bootstrap"
 import { useAuth } from "../contexts/AuthContext"
 import { Link, useHistory } from "react-router-dom"
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import Menu from '@material-ui/core/Menu';
+import Drawer from "@material-ui/core/Drawer";
+import Container from '@material-ui/core/Container';
+import Avatar from '@material-ui/core/Avatar';
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Box from "@material-ui/core/Box";
+import Tooltip from '@material-ui/core/Tooltip';
+import MenuItem from '@material-ui/core/MenuItem';
+
+
+
 
 export default function Dashboard() {
   const [error, setError] = useState("")
