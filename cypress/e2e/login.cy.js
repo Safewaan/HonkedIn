bcb6c2748960df6735465ccd8fbda947d22201b2
Safwaan Chowdhury <57375371+Safewaan@users.noreply.github.com>
2023-02-25 16:37:24 -0500

    Create e2e test for login page (#39)
    Co-authored-by: Safewaan <Safewaan@users.noreply.github.com>

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,49 @@
+describe('Login', () => {
+    it('Logs in with valid credentials', () => {
+      cy.visit('/login');
+  
+      // Fill out email and password inputs
+      cy.get('#email').type('TestAccount@HonkedIn.com');
+      cy.get('#password').type('password');
+  
+      // Submit the form
+      cy.get('form').submit();
+  
+      // Ensure user is redirected to homepage after login
+      cy.url().should('eq', 'http://localhost:3000/');
+    });
+  
+    it('Displays error message with invalid credentials', () => {
+      cy.visit('/login');
+  
+      // Fill out email and password inputs with invalid credentials
+      cy.get('#email').type('invalid@example.com');
+      cy.get('#password').type('invalid-password');
+  
+      // Submit the form
+      cy.get('form').submit();
+  
+      // Ensure error message is displayed
+      cy.get('.alert-danger').should('be.visible');
+    });
+  
+    it('Navigates to forgot password page', () => {
+      cy.visit('/login');
+  
+      // Click the "Forgot Password?" link
+      cy.contains('Forgot Password?').click();
+  
+      // Ensure user is redirected to the correct page
+      cy.url().should('eq', 'http://localhost:3000/forgot-password');
+    });
+  
+    it('Navigates to signup page', () => {
+      cy.visit('/login');
+  
+      // Click the "Sign Up" link
+      cy.contains('Sign Up').click();
+  
+      // Ensure user is redirected to the correct page
+      cy.url().should('eq', 'http://localhost:3000/signup');
+    });
+  });
