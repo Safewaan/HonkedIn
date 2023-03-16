973715c7053c17f324858c7941787dec2401217b
Esha Shah <67078410+Eshah7@users.noreply.github.com>
2023-03-15 20:21:03 -0400

    slay!

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(function (req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.header("Access-Control-Allow-Methods", "GET, OPTIONS, POST, PUT");
 	next();
 });
 
