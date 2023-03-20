168af58d7c3123f635f5ba9b9627a99e27e04e70
c45choi <c45choi@uwaterloo.ca>
2023-03-20 18:53:46 -0400

    added link

--- src/components/aboutUs/AboutUs.js
+++ src/components/aboutUs/AboutUs.js
@@ -4,6 +4,7 @@ import { Link, useHistory } from "react-router-dom"
 import "react-datepicker/dist/react-datepicker.css";
 import { useAuth } from "../../contexts/AuthContext"
 import Typography from "@material-ui/core/Typography";
+import { ExternalLinkIcon } from '@chakra-ui/icons'
 
 import {
     Alert,
@@ -35,15 +36,20 @@ const CreateResource = () => {
         <div>
             <NavigationBar></NavigationBar>
 
-            <Box maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden'>
+            <Card style={{ padding: '16px' }}>
                 <Text noOfLines={1} align="center" fontSize='5xl'>We are HonkedIn.</Text>
                 <Image
                     src={GOOSE_IMAGE}
                     align="center"
                     boxSize='200px' />
                 <Text align="center" fontSize='2xl'>We aim to connect students and scholars in the University of Waterloo Region.</Text>
+                <br></br>
+                <Text align="center" fontSize='2xl'>Want to learn more about how HonkedIn was built?</Text>
+                <Link href='https://github.com/Safewaan/HonkedIn' isExternal align="center" target = "_blank">
+                    Find out more here! <ExternalLinkIcon mx='2px' />
+                </Link>
 
-            </Box>
+            </Card>
 
 
         </div>
