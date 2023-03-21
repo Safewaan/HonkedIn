7a437b0b5da5202b6e0d5e76a02e1af02c0259c9
Esha Shah <67078410+Eshah7@users.noreply.github.com>
2023-03-20 22:43:47 -0400

    Added filters for forums

--- src/components/forums/Forums.js
+++ src/components/forums/Forums.js
@@ -7,6 +7,9 @@ import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import NavigationBar from '../common/NavigationBar';
 import Box from "@material-ui/core/Box";
+import DropdownFilter from "../common/filters/DropdownFilter";
+import ClearFilter from "../common/filters/ClearFilter";
+import Chip from '@material-ui/core/Chip';
 
 const { REACT_APP_API_ENDPOINT } = process.env;
 
@@ -84,6 +87,26 @@ const Forums = () => {
         return body;
     }
 
+    // Filters
+    const [forumTag, setForumTag] = React.useState('');
+    const forumTagList = ["School", "Co-op", "Funny", "Debate", "Rant", "Interview", "Class Review", "Good News"];
+
+    const handleForumTag = (event) => {
+        setForumTag(event.target.value);
+    }
+
+    const [status, setStatus] = React.useState('');
+    const statusList = ["Active", "Archived"];
+
+    const handleStatus = (event) => {
+        setStatus(event.target.value);
+    }
+
+    const handleRefreshFilter = async () => {
+        setForumTag("");
+        setStatus("");
+    }
+
     return (
         <div id="body">
 
@@ -98,30 +121,72 @@ const Forums = () => {
                 </Typography>
             </Box>
 
-    <Box sx={{ position: 'absolute', top: 180, left: '50%', transform: 'translateX(-50%)' }}>
-        {forums.map((forum) => (
-            <Card style={{ width: '800px', marginBottom: '20px' }} key={forum.id}>
-                <CardContent>
-                    <Link to={`/forum/${forum.id}`} target="_blank">
-                        <Typography variant="h5" component="div">
-                            {forum.forumTitle}<br />
-                        </Typography>
-                    </Link>
-                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                        Posted on {new Date(new Date(forum.dateTime).getTime() - (5 * 60 * 60 * 1000)).toLocaleDateString()}<br />
-                        by {forum.creatorName}<br />
-                    </Typography>
-                    <Typography variant="subtitle2" sx={{ mb: 1.5 }} color="text.secondary">
-                                Status: {forum.status}<br />
-                            </Typography>
-                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                        <br />{forum.description}<br />
-                    </Typography>
-                </CardContent>
-            </Card>
-        ))}
-        </Box>
-    </div>
+            <Box sx={{ width: '30%', position: 'absolute', top: 185, left: '50%', transform: 'translateX(-50%)', marginBottom: '20px' }}>
+
+                <Typography
+                    style={{ color: "black", mb: 2, fontSize: 14, align: 'right' }}
+                >
+                    Filters
+                </Typography>
+                <DropdownFilter
+                    placeholder="Select a Forum Tag"
+                    value={forumTag}
+                    onChange={handleForumTag}
+                    lists={forumTagList}
+                />
+                <br />
+                <DropdownFilter
+                    placeholder="Select the Status"
+                    value={status}
+                    onChange={handleStatus}
+                    lists={statusList}
+                />
+                <ClearFilter
+                    onClick={() => handleRefreshFilter()}
+                />
+
+            </Box>
+
+            <Box sx={{ position: 'absolute', top: 350, left: '50%', transform: 'translateX(-50%)' }}>
+                {forums.map((forum) => {
+                    if (forumTag && forum.forumTag !== forumTag) {
+                        return null;
+                    }
+                    if (status && forum.status !== status) {
+                        return null;
+                    }
+
+                    return (
+                        <Card style={{ width: '800px', marginBottom: '20px' }} key={forum.id}>
+                            <CardContent>
+                                <Link to={`/forum/${forum.id}`} target="_blank">
+                                    <Typography variant="h5" component="div">
+                                        {forum.forumTitle}<br />
+                                    </Typography>
+                                </Link>
+                                {forum.forumTag && <Chip
+                                    key={forum.id}
+                                    label={forum.forumTag}
+                                    color="primary"
+                                    size="small"
+                                    style={{ marginRight: 8 }}
+                                />}
+                                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                                    Posted on {new Date(new Date(forum.dateTime).getTime() - (5 * 60 * 60 * 1000)).toLocaleDateString()}<br />
+                                    by {forum.creatorName}<br />
+                                </Typography>
+                                <Typography variant="subtitle2" sx={{ mb: 1.5 }} color="text.secondary">
+                                    Status: {forum.status}<br />
+                                </Typography>
+                                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                                    <br />{forum.description}<br />
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    );
+                })}
+            </Box>
+        </div>
     )
 }
 
--- src/routes/forums/getForums.js
+++ src/routes/forums/getForums.js
@@ -9,7 +9,7 @@ router.get('/api/getForums', (req, res) => {
 	let connection = mysql.createConnection(config);
 
 	let sql = 
-	`SELECT CONCAT(firstName, " ", lastName) AS creatorName, forumTitle, forums.description, forums.dateTime, forums.id, forums.status
+	`SELECT CONCAT(firstName, " ", lastName) AS creatorName, forumTitle, forums.description, forums.dateTime, forums.id, forums.status, forums.forumTag
     FROM forums, users
     WHERE creatorID = users.id;`; 
 	
