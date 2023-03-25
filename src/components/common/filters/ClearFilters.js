87a30803458f215a1beb17fcf2a521fd5b537c46
Safewaan <Safewaan@users.noreply.github.com>
2023-03-25 02:23:07 -0400

    styling for clear filter

--- src/components/common/filters/ClearFilters.js
+++ src/components/common/filters/ClearFilters.js
@@ -1,17 +1,16 @@
-import { Box, Button} from '@chakra-ui/react'
-import "../../../styles/search-style.css";
+import { Box, Link } from '@chakra-ui/react'
 
 const ClearFilters = ({ onClick}) => {
     return (
-        <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-start' }}>
-            <Button
-                variant="link"
+        <Box 
+        sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-start' }}>
+            <Link
                 onClick={onClick}
-                className="search-reset"
+                className="link"
                 _hover={{ textDecor: "none" }}
             >
                 Clear Filters
-            </Button>
+            </Link>
         </Box>
 
     )
