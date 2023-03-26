fc3a8aa1abe96ac697d648b37b00ebaf0bbaf18a
Esha Shah <67078410+Eshah7@users.noreply.github.com>
2023-03-26 14:43:19 -0400

    MyComments Unit Test

---
+++ tests/componentsTest/MyComments.js
@@ -0,0 +1,136 @@
+import React, { useRef, useState, useEffect } from "react"
+//import { useAuth } from "../../contexts/AuthContext"
+import { Link, useHistory } from "react-router-dom"
+import { Card } from "react-bootstrap"
+
+import {
+    Box,
+    Text
+} from "@chakra-ui/react";
+
+//import NavigationBar from '../common/NavigationBar';
+
+const { REACT_APP_API_ENDPOINT } = process.env;
+
+const MyComments = ({loadGetComments, myComments}) => {
+
+    //const { currentUser } = useAuth();
+    const history = useHistory();
+    const [email, setEmail] = React.useState('');
+    const [userID, setUserID] = React.useState('');
+
+    //const [myComments, setMyComments] = React.useState([]);
+
+    useEffect(() => {
+        loadGetComments();
+    }, [loadGetComments()]);
+
+   /* React.useEffect(() => {
+        setEmail(currentUser.email);
+        loaduserSearchByEmail(currentUser.email);
+        //loadGetForums(); 
+    }, []);
+
+    const loaduserSearchByEmail = (email) => {
+        callApiGetuserSearchByEmail(email)
+            .then(res => {
+                var parsed = JSON.parse(res.express);
+                //console.log(parsed[0].id);
+                setUserID(parsed[0].id);
+            });
+    }
+
+    const callApiGetuserSearchByEmail = async (email) => {
+        const url = `${REACT_APP_API_ENDPOINT}/userSearchByEmail`;
+        //console.log(url);
+
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                email: email
+            })
+        });
+        const body = await response.json();
+        if (response.status !== 200) throw Error(body.message);
+        return body;
+    }
+    */
+
+    /* useEffect(() => {
+        loadGetCommentsByUserID();
+    }, [userID]);
+    */
+
+    /* const loadGetCommentsByUserID = async () => {
+        try {
+            const res = await CallApiGetCommentsByUserID(userID);
+            const parsed = res.express;
+            setMyComments(parsed);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    */
+
+    const CallApiGetCommentsByUserID = async (userID) => {
+
+        const url = `${REACT_APP_API_ENDPOINT}/getForumCommentsByUserID?userID=${userID}`;
+        console.log(url);
+
+        const response = await fetch(url, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        const body = await response.json();
+        if (response.status !== 200) throw Error(body.message);
+        return body;
+    }
+
+    return (
+        <div >
+
+           {/* <NavigationBar></NavigationBar>*/} 
+
+            <Box sx={{ position: 'absolute', top: 115, left: '50%', transform: 'translate(-50%, -50%)' }}>
+                <Text
+                    className="title"
+                >
+                    My Comments
+                </Text>
+            </Box>
+
+            <Box sx={{ position: 'absolute', top: 185, left: '50%', transform: 'translateX(-50%)' }}>
+                {myComments.map((comment) => (
+                    <Card style={{ width: '600px', marginBottom: '8px', padding: "16px" }} key={comment.id}>
+                        {/* <Link to={`/forum/${comment.forumID}`} target="_blank"> */} 
+                            <Text className="headerBig to-text">
+                                {comment.forumTitle}
+                            </Text>
+                       {/* </Link>*/}  
+
+                        <Text
+                            className="header to-text"
+                            marginTop="8px"
+                        >
+                            Commented on {new Date(new Date(comment.commentDateTime).getTime() - (5 * 60 * 60 * 1000)).toLocaleString()}
+                        </Text>
+
+                        <Text
+                            className="header to-text"
+                            marginTop="8px"
+                        >
+                            {comment.comment}
+                        </Text>
+                    </Card>
+                ))}
+            </Box>
+        </div>
+    )
+}
+
+export default MyComments; 
---
+++ tests/jestTests/MyComments.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import MyComments from '../componentsTest/MyComments'
+
+import '@testing-library/jest-dom/extend-expect';
+
+
+describe('<MyComments />', () => {
+
+    it('loads comments on first render on myComments page', () => {
+        const loadGetComments = jest.fn().mockName('loadGetComments');
+        const myComments = [];
+        render(<MyComments loadGetComments={loadGetComments} myComments={myComments} />);
+        expect(loadGetComments).toHaveBeenCalled();
+    });
+
+  it('displays the time and date of the comment on myComments page', () => {
+
+    const noop = () => { };
+
+    const myComments = [
+      {
+        "id": 1, "forumID": 1, "userID": 2, "commentDateTime": "2023-02-23T05:00:00.000Z", "comment": "Test 2"
+      }
+    ]
+
+    render(<MyComments loadGetComments={noop} myComments={myComments} />);
+    const string = new Date(new Date('2023-02-23T05:00:00.000Z').getTime() - (5 * 60 * 60 * 1000)).toLocaleString();
+    expect(screen.getByText(`Commented on ${string}`)).toBeInTheDocument();
+  });
+
+});
